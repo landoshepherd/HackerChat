8f51226fb92d45f395e1ef272ea61a83923e548b
landoshepherd <landolshepherd@gmail.com>
2023-10-14 15:17:29 -0400

    Rewrote client code as a class

--- Client/Client.hpp
+++ Client/Client.hpp
@@ -1,28 +1,37 @@
 #include <iostream>
-#include <boost/array.hpp>
 #include <boost/asio.hpp>
-#include <thread>
 #include <chrono>
+#include <iostream>
+#include <thread>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
 
-using boost::asio::ip::tcp;
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace websocket = beast::websocket;
+namespace net = boost::asio;
+using tcp = boost::asio::ip::tcp;
 
 class Client{
 public:
     Client();
     ~Client();
-    bool InitializeClient(std::string& serverName, std::string& serviceName);
+    bool Initialize();
     bool Start();
 
 private:
-    std::shared_ptr<boost::asio::io_context> m_context;
-    std::shared_ptr<tcp::resolver> m_tcpResolver;
-    tcp::resolver::results_type m_endPoints;
-    std::shared_ptr<tcp::socket> m_socket;
+    std::shared_ptr<boost::asio::io_context> m_ioContext;
+    std::shared_ptr<tcp::resolver> m_Resolver;
+    tcp::resolver::results_type m_EndPoints;
+    std::shared_ptr<tcp::socket> m_Socket;
+    std::shared_ptr<websocket::stream<tcp::socket>> m_WebSocketStream;
     std::vector<char> m_buffer;
-    bool started;
     boost::system::error_code m_error;
+    std::string m_destinationHost;
+    std::string m_destinationPort;
 
 private:
-    bool ResolveEndPoints(std::string& serviceName);
-    bool ConnectToSocket();
+    bool ConnectToServer();
 };
